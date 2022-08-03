fb0a91a4e65c1262549a365fad6867e1aa6ade30
Jonatan <mr-jona@hotmail.com>
2022-08-03 12:49:36 +0200

    finish search page filter

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -5,12 +5,31 @@ const db = require("../db.json");
 
 const Search = () => {
   const [articles, setArticles] = useState("");
+  const [filterText, setFilterText] = useState("nothing yet");
+
+  const filterChanger = (e) => {
+    //change the state of the input value
+    setFilterText(e.target.value);
+  };
 
   const renderArticleList = !articles
     ? "no articles found"
-    : articles.map((article) => {
-        return <ArticleCard object={article} />;
-      });
+    : articles
+        .filter((article) => {
+          return (
+            //filter on title or author or category if user writes lowercase
+            article.title.toLowerCase().includes(filterText) ||
+            article.author.toLowerCase().includes(filterText) ||
+            article.categoryId.toLowerCase().includes(filterText) ||
+            // filter on the same but sensitive to caps
+            article.title.includes(filterText) ||
+            article.author.includes(filterText) ||
+            article.categoryId.includes(filterText)
+          );
+        })
+        .map((article) => {
+          return <ArticleCard object={article} />;
+        });
 
   useEffect(() => {
     //normally this would be an axios call to the server, using it this way now to work without wifi
@@ -20,9 +39,15 @@ const Search = () => {
 
     console.log("articles", articles);
   }, []);
+
   return (
     <div id="searchPage">
-      <h1>this is the search page</h1>
+      <input
+        onChange={filterChanger}
+        id="articleFilterInput"
+        type="text"
+        value={filterText}
+      />
       <div id="articleListDiv">{renderArticleList}</div>
     </div>
   );
