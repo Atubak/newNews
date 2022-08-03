82bbfa9c919da54f719521bd3628204f8eb6d64a
Jonatan <mr-jona@hotmail.com>
2022-08-03 12:04:55 +0200

    start search page work with importing the hardcoded db, forgot to switch branches

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,4 +1,12 @@
+import { useEffect } from "react";
+
+const db = require("../db.json");
+
 const Search = () => {
+  useEffect(() => {
+    //normally this would be an axios call to the server, using it this way now to work without wifi
+    console.log(db);
+  }, []);
   return (
     <div id="searchPage">
       <h1>this is the search page</h1>
